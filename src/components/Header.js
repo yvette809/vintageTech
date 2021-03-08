72373660b2c681d1ff162f2a0921a862470995d8
Yvette <nchombuayvta@gmil.com>
2021-03-08 18:17:47 +0100

    start up files

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+import logo from '../assets/logo.svg'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={logo} alt="vintage tech logo" className="logo"/>
+      <nav>
+        <ul>
+          <div>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+
+          </div>
+          <div>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/cart">Cart</Link></li>
+
+          </div>
+        </ul>
+      </nav>
+    </header>
+  )
+}
---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,7 @@
+import React from "react";
+import {useParams} from 'react-router-dom'
+
+export default function ProductDetails() {
+  const {id} = useParams()
+  return <h1>hello from product details page. product id is {id}</h1>;
+}
